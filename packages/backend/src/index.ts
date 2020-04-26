ee4048fc503bf3df45a781c4efca08ba86e799c4
tahscenery <tahscenery@gmail.com>
2020-04-26 22:11:57 +1000

    Added essential packages: express, graphql, type-graphql, typeorm

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,20 @@
+import 'reflect-metadata';
+import express from 'express';
+import * as typeGraphQL from 'type-graphql';
+import * as typeorm from 'typeorm';
+
 export const sayHello = 'Hello, World!'
+const app = express();
+const port = 4000;
+
+(async () => {
+  // await typeorm.createConnection().catch(error => {
+  //   console.log(`Error: Failed to create connection to database: \`${error}\``)
+  // });
+
+  // const schema = await typeGraphQL.buildSchema({ resolvers: [] });
+
+  app.listen(port, () => {
+    console.log(`\nServer started on http://localhost:${port}/graphql\n`);}
+  );
+})();
