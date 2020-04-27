39807e191d06baaa5a2c6eddd0d2cd1ce524816a
Bryan Colin <bryancolin35@ymail.com>
2020-04-27 15:56:30 +0700

    Update Login
    Login Frontend Updated

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from 'utils/serviceWorker';
 
 import App from 'components/App';
+import Login from 'pages/Login/Login';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
