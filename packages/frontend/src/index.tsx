0efb2843dd25f5e343f4b32241699db45134617b
tahscenery <tahscenery@gmail.com>
2020-04-28 13:25:11 +1000

    Revert "Update Login"
    This reverts the commit made by Bryan.

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from 'utils/serviceWorker';
 
 import App from 'components/App';
-import Login from 'pages/Login/Login';
 import './index.css';
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
