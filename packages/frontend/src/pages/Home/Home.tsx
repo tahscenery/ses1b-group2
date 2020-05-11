a336ddd5147878ba102b5d19a5cba21cb9fdc43f
tahscenery <tahscenery@gmail.com>
2020-05-11 14:59:23 +1000

    Prototype home page

--- packages/frontend/src/pages/Home/Home.tsx
+++ packages/frontend/src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { Typography } from '@material-ui/core';
 
-import NavBar from 'components/NavBar'
+import './Home.css';
+import NavBar from 'components/NavBar';
+// import Footer from 'components/Footer';
 
 class Home extends Component {
   render() {
@@ -9,7 +11,12 @@ class Home extends Component {
       <div>
         <NavBar/>
         <div className="component-container">
-          <Typography variant="h2">Home</Typography>
+          <Typography variant="h2">Unique is in our name</Typography>
+          <img
+            style={{ width: "100%", paddingTop: "20px" }}
+            src={`https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80`}
+            alt="image"
+            />
         </div>
       </div>
     );
