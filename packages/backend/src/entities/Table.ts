72a92ea5c7b5438a380b24a230514f33aa547728
Prince <prince.prince.shrestha7@gmail.com>
2020-05-15 16:39:20 +1000

    Table model

---
+++ packages/backend/src/entities/Table.ts
@@ -0,0 +1,27 @@
+import { ObjectType, Field, ID } from "type-graphql";
+import { prop as Property, getModelForClass } from "@typegoose/typegoose";
+
+@ObjectType({ description: "The Table model " })
+export class Table {
+  [x: string]: any;
+  @Field(() => ID)
+  id: number;
+
+  @Field()
+  @Property({ required: true })
+  tableNumber: number;
+
+  @Field()
+  @Property({ required: true })
+  minCapacity: number;
+
+  @Field()
+  @Property({ required: true })
+  maxCapacity: number;
+
+  @Field()
+  @Property({ required: true })
+  description: String;
+}
+
+export const TableModel = getModelForClass(Table);
---
+++ packages/backend/src/resolvers/Table.ts
@@ -0,0 +1,42 @@
+import { Resolver, Mutation, Arg, Query } from "type-graphql";
+import { Table, TableModel } from "../entities/Table";
+import { TableInput } from "./types/Table-input";
+
+@Resolver((_of) => Table )
+export class TableResolver {
+  @Query((_returns) => Table, { nullable: false })
+  async returnSingleTable(@Arg("id") id: string) {
+    return await TableModel.findById({ _id: id});
+  }
+
+  @Query(() => [Table])
+  async returnAllTables() {
+    return await TableModel.find();
+  }
+
+  @Mutation(() => Table)
+  async createTable(@Arg("data") {tableNumber, minCapacity, maxCapacity, description}: TableInput): Promise<Table> {
+
+    const existingTable = TableModel.findOne( {tableNumber: tableNumber});
+    if(existingTable) 
+    {
+      throw new Error("Table exists already.");
+    }
+
+    const Table = (
+      await TableModel.create({ 
+        tableNumber,
+        minCapacity,
+        maxCapacity,
+        description
+      })
+    ).save();
+    return Table;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteTable(@Arg("id") id: string) {
+    await TableModel.deleteOne({ _id: id });
+    return true;
+  }
+}
