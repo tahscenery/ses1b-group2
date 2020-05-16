6272dd6ddae76501b2180c5c88d3c9ecf8ea70cc
tahscenery <tahscenery@gmail.com>
2020-05-16 16:32:13 +1000

    Temporarily removed Rico's slideshow component

--- packages/frontend/src/pages/Home/Home.tsx
+++ packages/frontend/src/pages/Home/Home.tsx
@@ -3,18 +3,17 @@ import React, { Component } from 'react';
 
 import './Home.css';
 import NavBar from 'components/NavBar';
-import Slideshow from 'components/Slider/Slide';
+// import Slideshow from 'components/Slider/Slide';
 // import Footer from 'components/Footer';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <NavBar/> 
-        <div className='attach'>
-        
-        <Slideshow/>
-        </div>
+        <NavBar/>
+        {/* <div className='attach'> */}
+          {/* <Slideshow/> */}
+        {/* </div> */}
       </div>
     );
   }
