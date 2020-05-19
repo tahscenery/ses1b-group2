2a8e5c61abe291298406d8ef3975746610006826
tahscenery <tahscenery@gmail.com>
2020-05-19 18:05:18 +1000

    Whitespace

--- packages/backend/src/connection.ts
+++ packages/backend/src/connection.ts
@@ -3,19 +3,16 @@ import mongoose from "mongoose";
 const uri = `mongodb+srv://bryancolin35:bryan@ss1b-papxc.mongodb.net/restaurant-dev?retryWrites=true&w=majority`;
 
 async function connectDB(): Promise<void> {
-  try 
-  {
+  try {
     await mongoose.connect(uri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDb connected...");
-  } 
-  catch (error) 
-  {
+    console.log("MongoDB successfully connected.");
+  } catch (error) {
     console.log(error);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
