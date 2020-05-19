33f9be82080dc4b63a28779974e622416b31b712
tahscenery <tahscenery@gmail.com>
2020-05-19 21:22:56 +1000

    Communicating with GraphQL

--- packages/frontend/src/pages/ViewMenu/ViewMenu.tsx
+++ packages/frontend/src/pages/ViewMenu/ViewMenu.tsx
@@ -1,31 +1,53 @@
 import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import { Typography } from '@material-ui/core';
+import gql from 'graphql-tag';
 
 import './ViewMenu.css';
 import NavBar from 'components/NavBar';
-import { menu, MenuItem } from 'resources/menu.json';
 
-interface ItemProps {
-  item: MenuItem;
+interface Item {
+  name: string;
+  description: string;
+  price: number;
+  category: "entree" | "main" | "dessert";
 }
 
-const Item = (props: ItemProps) => {
-  const { name, price, description } = props.item;
-  return (
-    <div className="view-menu-item">
-      <div className="view-menu-item-heading">
-        <Typography variant="h3">{name}</Typography>
-        <Typography variant="h3">{price}</Typography>
-      </div>
-      <p>{description}</p>
-    </div>
-  );
+interface ItemsData {
+  items: Item[];
 }
+
+const GET_ITEMS = gql`
+  query getItem {
+    items {
+      name
+      description
+      price
+      category
+    }
+  }
+`;
 
 const ViewMenu = () => {
   useEffect(() => {
     document.title = 'Menu – Sapori Unici';
   });
+
+  const { loading, error, data } = useQuery<ItemsData>(GET_ITEMS);
+
+  if (loading) {
+    console.log("Loading...");
+  } else {
+    if (error) {
+      console.log(`An error occurred: ${error.message}`);
+    } else if (!data) {
+      console.log("No data...");
+    } else {
+      data.items.forEach(item => {
+        console.log({ name: item.name, description: item.description, price: `$${item.price}` })
+      });
+    }
+  }
 
   return (
     <div>
@@ -34,21 +56,21 @@ const ViewMenu = () => {
         <div className="view-menu">
           <div className="view-menu-collection">
             <Typography variant="h2">Entrée & Salads</Typography>
-            {menu
+            {/* {menu
               .filter(item => item.type === 'entree')
-              .map((item, index) => <Item key={`Item#${index}`} item={item}/>)}
+              .map((item, index) => <Item key={`Item#${index}`} item={item}/>)} */}
           </div>
           <div className="view-menu-collection">
             <Typography variant="h2">Mains</Typography>
-            {menu
+            {/* {menu
               .filter(item => item.type === 'main')
-              .map((item, index) => <Item key={`Item#${index}`} item={item}/>)}
+              .map((item, index) => <Item key={`Item#${index}`} item={item}/>)} */}
           </div>
           <div className="view-menu-collection">
             <Typography variant="h2">Desserts & Drinks</Typography>
-            {menu
+            {/* {menu
               .filter(item => item.type === 'dessert')
-              .map((item, index) => <Item key={`Item#${index}`} item={item}/>)}
+              .map((item, index) => <Item key={`Item#${index}`} item={item}/>)} */}
           </div>
         </div>
       </div>
