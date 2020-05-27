0c09efdd2249e5df3f6b71f2afac513d899e506d
tahscenery <tahscenery@gmail.com>
2020-05-27 17:56:36 +1000

    Converted NavBar to functional component

--- packages/frontend/src/components/NavBar/NavBar.tsx
+++ packages/frontend/src/components/NavBar/NavBar.tsx
@@ -1,29 +1,19 @@
-import React, { Component } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, IconButton, Typography } from '@material-ui/core'
 import Menu from '@material-ui/icons/Menu';
 import Close from '@material-ui/icons/Close'
 
 import './NavBar.css';
 
-interface NavBarProps {}
+const NavBar = () => {
+  const [shouldShowMenu, setShouldShowMenu] = useState(false);
 
-interface NavBarState {
-  shouldShowMenu: boolean;
-}
-
-class NavBar extends Component<NavBarProps, NavBarState> {
-  constructor(props: NavBarProps) {
-    super(props);
-    this.state = { shouldShowMenu: false };
-    this.toggleMenu = this.toggleMenu.bind(this)
-  }
-
-  toggleMenu() {
-    this.setState({ shouldShowMenu: !this.state.shouldShowMenu });
+  const toggleMenu = () => {
+    setShouldShowMenu(!shouldShowMenu);
     const navBarMenu = document.getElementById('nav-bar-menu');
 
-    if (navBarMenu !== null && navBarMenu !== undefined) {
-      if (this.state.shouldShowMenu) {
+    if (navBarMenu) {
+      if (shouldShowMenu) {
         navBarMenu.classList.add('collapsed');
       } else {
         navBarMenu.classList.remove('collapsed');
@@ -31,34 +21,32 @@ class NavBar extends Component<NavBarProps, NavBarState> {
     }
   }
 
-  render() {
-    return (
-      <div className="nav-bar-container">
-        <div className="nav-bar">
-          <div className="nav-bar-contents">
-            <div className="nav-bar-persistent">
-              <Typography variant="h1">
-                <a href="/" className="nav-bar-brand-link">Sapori Unici</a>
-              </Typography>
-              <span className="nav-bar-collapse-icon">
-                <IconButton color="primary" onClick={this.toggleMenu}>
-                  {this.state.shouldShowMenu ? <Close/> : <Menu/>}
-                </IconButton>
-              </span>
-            </div>
-            <nav id="nav-bar-menu" className="nav-bar-menu collapsed">
-              <ul>
-                <li><Button variant="outlined" color="primary" href="/menu">Our Menu</Button></li>
-                <li><Button variant="outlined" color="primary" href="/locations">Locations</Button></li>
-                <li><Button variant="outlined" color="primary" href="/register">Sign Up</Button></li>
-                <li><Button variant="outlined" color="primary" href="/login">Login</Button></li>
-              </ul>
-            </nav>
+  return (
+    <div className="nav-bar-container">
+      <div className="nav-bar">
+        <div className="nav-bar-contents">
+          <div className="nav-bar-persistent">
+            <Typography variant="h1">
+              <a href="/" className="nav-bar-brand-link">Sapori Unici</a>
+            </Typography>
+            <span className="nav-bar-collapse-icon">
+              <IconButton color="primary" onClick={toggleMenu}>
+                {shouldShowMenu ? <Close/> : <Menu/>}
+              </IconButton>
+            </span>
           </div>
+          <nav id="nav-bar-menu" className="nav-bar-menu collapsed">
+            <ul>
+              <li><Button variant="outlined" color="primary" href="/menu">Our Menu</Button></li>
+              <li><Button variant="outlined" color="primary" href="/locations">Locations</Button></li>
+              <li><Button variant="outlined" color="primary" href="/register">Sign Up</Button></li>
+              <li><Button variant="outlined" color="primary" href="/login">Login</Button></li>
+            </ul>
+          </nav>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default NavBar;
